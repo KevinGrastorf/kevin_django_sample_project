6f3465f3612ea810b0266c54ea16fb48a6ad21c8
Kevin Grastorf <kevin.grastorf@gmail.com>
2020-07-19 21:37:50 -0700

    New Changes

--- portfolio_new/settings.py
+++ portfolio_new/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
@@ -127,3 +129,5 @@ MEDIA_ROOT = BASE_DIR
 
 URL_URL = '/url/'
 URL_ROOT = BASE_DIR
+
+django_heroku.settings(locals())

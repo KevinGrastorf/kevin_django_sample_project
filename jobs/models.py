d7033ed96ed370609139126180ff1df8891abbad
Kevin Grastorf <kevin.grastorf@gmail.com>
2020-07-19 22:30:31 -0700

    Updated Media to CharField

--- jobs/models.py
+++ jobs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image = models.CharField(max_length=200)
     summary = models.CharField(max_length=200)
     url = models.CharField(max_length=200, default='url')
 
